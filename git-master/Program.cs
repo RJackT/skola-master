2ef3c28bfcb72b72ce0f20f39ecfdc1f52fe3b6f
RJackT <91957409+RJackT@users.noreply.github.com>
2022-08-26 13:43:05 +0200

    initial program

---
+++ git-master/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Skriv en boolean.");
+string input;
+do
+{
+    input = Console.ReadLine();
+}while(input  == null);
+
+if (input.ToLower() == "true")
+    Console.Write("True");
+else if (input.ToLower() == "false")
+    Console.Write("False");
+else
+    Console.Write("Your input was not valid.");
